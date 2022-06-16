4d3604ae0b7cc75d6a24cc8a101de9dea24017f2
Ewa Domańska <ewadomanska0510@gmail.com>
2022-06-16 21:42:48 +0200

    Task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,24 +1,22 @@
 let startButton = document.querySelector('[data-start]');
 let stopButton = document.querySelector('[data-stop]');
 let background = document.querySelector("body");
+let intervalID;
 
 startButton.addEventListener("click", evt => {
-  setInterval(() => {
-    background.style.backgroundColor = getRandomHexColor();
+  intervalID = setInterval(() => {
+      background.style.backgroundColor = getRandomHexColor();
   }, 1000);
   startButton.disabled = true;
+  stopButton.disabled = false;
 });
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-
-// function () {
-//
-// }
-//
-//
-// () => {
-//
-// }
+stopButton.addEventListener("click", evt => {
+  clearInterval(intervalID);
+  stopButton.disabled = true;
+  startButton.disabled = false;
+});
