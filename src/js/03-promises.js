c9197f19adab26fb8876aaef3c4eb44d5a8e8c38
Ewa Domańska <ewadomanska0510@gmail.com>
2022-06-25 21:16:02 +0200

    task 3 in progress

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,34 @@
+import Notiflix from 'notiflix';
+
+let firstDelay = document.getElementsByName("delay")[0];
+let step = document.getElementsByName("step")[0];
+let amount = document.getElementsByName("amount")[0];
+let button = document.querySelector("button")
+
+
+button.addEventListener("click", evt => {
+  let delay = firstDelay.valueAsNumber;
+  for (let position = 1; position <= amount.valueAsNumber; position++) {
+    createPromise(position, delay)
+      .then(({position, delay}) => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({position, delay}) => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delay = delay + step.valueAsNumber;
+  }
+})
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({position, delay});
+      } else {
+        reject({position, delay});
+      }
+    }, delay);
+  });
 }
